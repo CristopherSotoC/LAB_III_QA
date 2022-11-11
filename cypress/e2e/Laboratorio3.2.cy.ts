7805fe4e91fc6c285fbacc629c66aa93294b472b
CristopherSC <cristophersc2815956@estudiantec.cr>
2022-11-11 16:56:19 -0600

    Commit1.2

--- cypress/e2e/Laboratorio3.2.cy.ts
+++ cypress/e2e/Laboratorio3.2.cy.ts
@@ -1,27 +1,55 @@
 describe('Clients', function () {
     beforeEach(function () {
-        cy.visit('https://www.bnventadebienes.com/properties/search');
+        cy.visit('https://www.bnventadebienes.com/vehicles/search');
     });
 
     /*
-    Nombre de la prueba: Descripcion con espacio blanco en Clients
-    Objetivo: Probar la funcionalidad agregando un Client
-    Datos de prueba: Description: ----
-    Resultado esperado: Exito ya que al ingresar un Clients sin descripcion deberia de 
-                        devolver "This field is required"
+    Nombre de la prueba: Verificar el realizar una busqueda con un año no numerico
+    Objetivo: Probar la funcionalidad de buscar autos con un año no numerico
+    Datos de prueba: 
+                    Año: 'AAAA'
+    Resultado esperado: Que al digitar un año no numerico, salte el aviso de "El campo Año debe ser un número."
     */
-    it('Description con espacio en blanco en Clients y en Type', function () {
+    it('Verificar el realizar una busqueda con un año no numerico', function () {
+        
+        cy.get('#VehicleYear').type('AAAA');
 
+        cy.get('.btn.btn-primary-action').click();
+
+        cy.get('#VehicleYear-error').should(
+            'contain.text',
+            'El campo Año debe ser un número.'
+          );
+        
     });
-
     /*
-    Nombre de la prueba: Descripcion con espacio blanco en Clients
-    Objetivo: Probar la funcionalidad agregando un Client
-    Datos de prueba: Description: ----
-    Resultado esperado: Exito ya que al ingresar un Clients sin descripcion deberia de 
-                        devolver "This field is required"
+    Nombre de la prueba: Verificar Busqueda con datos Validos
+    Objetivo: Comprobar si una busqueda con datos Validos funciona
+    Datos de prueba: 
+                    Año: '2010'
+                    Estado del Vehículo: valor 1 = 'Disponible'
+                    Tipo de venta: valor 1 = 'Subasta'
+                    Precio minimo: '1000000'
+                    Precio maximo: '10000000'
+    Resultado esperado: Permitir la busqueda, y mostrarnos un Label con la frase 'Resultados de la búsqueda
     */
-    it('Description con espacio en blanco en Clients y en Type', function () {
+
+    it('Verificar Busqueda con datos Validos', function () {
+        cy.get('#VehicleYear').type('2010');
+
+        cy.get('.row > :nth-child(1)   >:nth-child(1) .col-md-12 > select').select(1)
+
+        cy.get('.row > :nth-child(1)  >:nth-child(2) .col-md-12 > select').select(1)
+
+        cy.get('#MinPrice').type('1000000');
+        cy.get('#MaxPrice').type('10000000');
+
+        cy.get('.btn.btn-primary-action').click();
+
+        cy.get('.row .col-sm-12 .brand-main-color3.main-title').should(
+            'contain.text',
+            'Resultados de la búsqueda'
+          );
 
     });
     
